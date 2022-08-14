7933656583789824beb4b0efecda10bd7542656b
Kolja Nagel <kn685832@fh-muenster.de>
2022-08-14 15:53:31 +0200

    Added python swagger api

--- app/main.py
+++ app/main.py
@@ -14,7 +14,7 @@ def get_color_thief_image(url):
     return ColorThief('image.jpg')
 
 
-@app.route("/", methods=['GET'])
+@app.route("/spotify-api/Color/Average", methods=['GET'])
 def get_avg_color():
     img = cv2.cvtColor(io.imread(request.args['img']), cv2.COLOR_BGR2RGB)
     avg_color_per_row = numpy.average(img, axis=0)
@@ -25,7 +25,7 @@ def get_avg_color():
     return resp
 
 
-@app.route("/dominant", methods=['GET'])
+@app.route("/spotify-api/Color/Dominant", methods=['GET'])
 def get_dominant_color():
     dominant_color = get_color_thief_image(request.args['img']).get_color(quality=10)
     return Response(response=f'{{"r": {dominant_color[0]}, "g": {dominant_color[1]}, "b": {dominant_color[2]}}}',
@@ -33,7 +33,7 @@ def get_dominant_color():
                     mimetype="application/json")
 
 
-@app.route("/palette", methods=['GET'])
+@app.route("/spotify-api/Color/Palette", methods=['GET'])
 def get_dominant_color_palette():
     color_palette = get_color_thief_image(request.args['img']).get_palette(quality=10, color_count=2)
     json = f'[{{"r": {color_palette[0][0]}, "g": {color_palette[0][1]}, "b": {color_palette[0][2]}}},' \
